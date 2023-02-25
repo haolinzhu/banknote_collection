a9c36d6ea982e465ef27cb3f3aba532a82b1f496
haolinzhu <haolinz@berkeley.edu>
2023-02-24 22:44:54 -0800

    first view

--- banknotes/views.py
+++ banknotes/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-# from .models import Banknote
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Banknote
+
 
 # Create your views here.
 def index(request):
-    # banknotes = Banknote.objects.all()
-    # return render(request, 'banknotes/index.html', {'banknotes': banknotes})
+    country_list = Banknote.objects.values_list('country', flat=True).distinct().order_by('country')
+    template = loader.get_template('banknotes/index.html')
+    context = {
+        'country_list': country_list,
+    }
+    return HttpResponse(template.render(context, request))
 
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+def banknotes(request, country):
+    return HttpResponse("You're looking at banknotes of %s." % country)
